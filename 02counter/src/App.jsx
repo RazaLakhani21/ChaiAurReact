80e35ffe93a86498722f14c1f8455c9cd73ea097
Lakhani Mohammad Raza <razalakhani21@gmail.com>
2024-08-08 18:49:58 +0530

    Made 'Counter' project using React and also Complete the assignment of not accepting counter less than '0', means no negatives allowed

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,23 +4,21 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  useState(); //the value inside "useState()" could be anything :- it could be '[]', '{}', 'true', 'false' and many more etc... , as for now we're putting "15" as we need it as default value.
 
-  useState()//the value inside "useState()" could be anything :- it could be '[]', '{}', 'true', 'false' and many more etc... , as for now we're putting "15" as we need it as default value.
-
-  let [counter, setCounter] = useState(15) //on 0th index the value could be anything 'counter' or 'SuperMan' and on the 1st index it is Function. and because of name-Convention we've put the name 'setCounter' not necessary, it could be 
+  let [counter, setCounter] = useState(1); //on 0th index the value could be anything 'counter' or 'SuperMan' and on the 1st index it is Function. and because of name-Convention we've put the name 'setCounter' not necessary, it could be
   // "let [counter, 'HiteshCounter'] " hitesh counter also ****.
 
   // The "setCounter" is a function responsible to Update the "counter" (variable) value.
 
-  // let counter = 15; now we don't need this part as it won't be propagating into the UI. 
-  
+  // let counter = 15; now we don't need this part as it won't be propagating into the UI.
+
   const addValue = () => {
     // console.log("Value Added !", Math.random()) for first trial, as just for checking !!
     // console.log("Clicked !", counter);
     // counter = counter + 1;
     // setCounter(counter) OR
-    setCounter(counter + 1)
-
+    setCounter(counter + 1);
   };
 
   // const singuClicku = () => {
@@ -28,19 +26,22 @@ function App() {
   // }  Just for Fun 😂😂
 
   const removeValue = () => {
+    if (counter > 0)
     // console.log(`The Value has been Removed or Decreased by 1 ${counter}`)
-    setCounter(counter - 1)
-  }
+    setCounter(counter - 1);
+  };
 
   return (
     <>
-    //just for fun I've put this "{counter}" in different places !
+      //just for fun I've put this "{counter}" in different places !
       <h1> Chai aur React</h1>
       <h2>Counter Value: {counter}</h2>
-
-      <button 
-      // onClick={singuClicku}
-       onClick={addValue}>Add Value {counter}</button>
+      <button
+        // onClick={singuClicku}
+        onClick={addValue}
+      >
+        Add Value {counter}
+      </button>
       <br />
       <button onClick={removeValue}>Remove Value {counter}</button>
       <p>footer: {counter}</p>
